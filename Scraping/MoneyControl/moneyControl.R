7c1145447f044770c51b25a29dd8cb9a361a0df4
Tanmay Khandait <DaitTan@DaitTan.daittan>
2018-12-11 09:21:06 +0530

    Modified MoneyControl script to store data in a CSV format.

--- Scraping/MoneyControl/moneyControl.R
+++ Scraping/MoneyControl/moneyControl.R
@@ -7,61 +7,93 @@ library(rvest)
 library(RCurl)
 library(XML)
 
-data<-data.frame()
 
 # Defining the search Term
 term = "gold"
 term = gsub(" ", "-", term, fixed = TRUE)
 
+# Creating the base adddress/home page for scraping
 base_addr = paste0("https://www.moneycontrol.com/news/tags/",term,".html")
-# Page we want to scrape from 
+
+# Extracting HTML of the page we want to scrape from 
 page <- read_html(base_addr)
 
+# Finding the last page of available data
 last = tail(page %>% html_nodes('div.pagenation') %>% html_nodes('a') %>% html_attr('data-page'),1)
+
+# Creating links for all pages to scrape from 
 pages = matrix()
 for(iter in 1:last){
   pages[iter] = paste0(base_addr,"/page-",iter,"/")
   
 }
 
+# Initialzing the dataframe that will hold the data
+testData = data.frame()
 
-# FOlder where all scraped data will be stored
-dir.create(file.path("D:/IGPC/NewsScraper", "MoneyControl"), showWarnings = FALSE)
-setwd(file.path("D:/IGPC/NewsScraper", "MoneyControl"))
 
-for (pageIterate in 98:last){
-  wdName = paste0("MoneyControl",pageIterate)
-  dir.create(file.path("D:/IGPC/NewsScraper/MoneyControl", wdName), showWarnings = FALSE)
-  setwd(file.path("D:/IGPC/NewsScraper/MoneyControl", wdName))
+# Scraping process
+
+# Loop to iterate through pages Page 1, Page 2, ... , Page N
+for (pageIterate in 1:last){
   
   
   # Link of every page from which we will scrape data
-  
   page = read_html(pages[pageIterate])
   
+  # Extracting all articles on the page
   allArticleNodes = page %>% html_nodes('ul') %>% html_nodes('li.clearfix')
   
+  # Loop to iterate through articles on one page
   for(iterate in 1 : length(allArticleNodes)){
+    
+    # Creating rows of Information
+    
+    # rowInf will be a vector of the following format
+    # | Link | Headlines | Date | Text |
+    
     rowInf = c((allArticleNodes %>% html_nodes('h2') %>% html_nodes('a') %>% html_attr('href'))[iterate],
                (allArticleNodes %>% html_nodes('h2') %>% html_nodes('a') %>% html_attr('title'))[iterate],
                (allArticleNodes %>% html_nodes('span') %>% html_text())[iterate])
     
+    # We will extract text only if a the link for the article exists
     if(url.exists(rowInf[1])==TRUE){
       
-      
+      # Reading HTML page
       explore <- read_html(rowInf[1])
       
+      # Subheading and text of the news Article
       subHead = explore %>% html_nodes('article.article_box') %>%html_nodes('h2.subhead')%>% html_text()
+      newsText =explore %>% html_nodes('article.article_box') %>%html_nodes('div.arti-flow') %>% html_nodes('p') %>% html_text()
       
-      text =explore %>% html_nodes('article.article_box') %>%html_nodes('div.arti-flow') %>% html_nodes('p') %>% html_text()
+      # Pasting Subheading and Text together
+      text = paste(subHead,newsText,collapse=" ")
       
-      rowInf <- append(append(rowInf, subHead),text)
+      # Appening the text to the rowInf
+      rowInf <- append(rowInf,text)
       
     }
     
-    fileName = paste0("MoneyControlNews",iterate,".txt")
-    write.table(rowInf,file=fileName,row.names=FALSE)
-    rowInf = NULL
-    paste("news" , iterate , "saved")
+    # In case the text doesnt exist, NA is added
+    length(rowInf) <- 4
+    
+    # Adding data to dataFrame
+    newData = rbind(newData2,t(as.matrix(rowInf)))
+    message("Article ", iterate)
+    
+    # Resetting the rowInf for next article
+    rowInf=NULL
   }
+  message("Page ", pageIterate)
+  message("*********** ", pageIterate)
+  message("*********** ", pageIterate)
+  message("*********** ", pageIterate)
 }
+
+
+# Renaming columns of the dataFrame
+colnames(testData) <- c("link", "headlines", "timeStamp", "text")
+
+# Saving DataFrame
+write.csv(testData, "moneyControlData.csv",row.names = FALSE)
+
