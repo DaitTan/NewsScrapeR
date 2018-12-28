6ef6548f34b79556b48f4dd8014a4d369d0f3296
Tanmay Khandait <DaitTan@DaitTan.daittan>
2018-12-28 18:04:20 +0530

    Added MarketsWatch.com scraping file

---
+++ Scraping/marketWatch/marketWatch.R
@@ -0,0 +1,27 @@
+library(rvest)
+
+
+
+page <- read_html("https://www.marketwatch.com/search?q=Gold&m=Keyword&rpp=100&mp=806&bd=false&rs=true")
+articles <- page %>% html_nodes('div.block')
+date <- articles %>% html_nodes('div.deemphasized') %>% html_nodes('span') %>% html_text()
+href <- articles %>% html_nodes('div.searchresult') %>% html_nodes('a') %>% html_attr('href')
+news <- articles %>% html_nodes('div.searchresult') %>% html_nodes('a') %>% html_text()
+marketsWatch <- as.data.frame(cbind(date,href,news))
+
+for(i in 1:670){
+  page <- read_html(paste0("https://www.marketwatch.com/search?q=Gold&m=Keyword&rpp=100&mp=806&bd=false&rs=true&o=", i,"01"))
+  articles <- page %>% html_nodes('div.block')
+  date <- articles %>% html_nodes('div.deemphasized') %>% html_nodes('span') %>% html_text()
+  href <- articles %>% html_nodes('div.searchresult') %>% html_nodes('a') %>% html_attr('href')
+  news <- articles %>% html_nodes('div.searchresult') %>% html_nodes('a') %>% html_text()
+  
+  marketsWatch <- rbind(marketsWatch, as.data.frame(cbind(date,href,news)))
+  date = NULL
+  href = NULL
+  news = NULL
+  
+  message(i)
+}
+  
+  
